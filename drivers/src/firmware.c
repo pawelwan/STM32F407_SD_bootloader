6da9328efea31f482393d0ea348471ba0737ae57
Pawel Wandzel <pawelwan@gmail.com>
2017-12-20 16:47:38 +0100

    add header parsing

--- drivers/src/firmware.c
+++ drivers/src/firmware.c
@@ -12,6 +12,7 @@
 
 typedef void (*pFunction)(void);
 static uint8_t upload_file(const char* name);
+static uint8_t parse_header(uint8_t *data, FirmwareHeader_t* header);
 
 FATFS fs;
 
@@ -35,25 +36,34 @@ uint8_t firmware_deinit(void) {
 }
 
 uint16_t firmware_curr_version(void) {
-    FirmwareHeader_t *header = (FirmwareHeader_t *) HEADER_ADDR;
-    return header->version;
+    uint8_t res;
+    FirmwareHeader_t header;
+
+    res = parse_header((uint8_t *) HEADER_ADDR, &header);
+    if (res != 1) return 0;
+
+    return header.version;
 }
 
 uint16_t firmware_new_version(void) {
     FRESULT res;
     FIL file;
     UINT bytes_read;
+    uint8_t buffer[HEADER_SIZE];
     FirmwareHeader_t header;
 
     memset(&file, 0, sizeof(file));
     res = f_open(&file, NEW_NAME, FA_OPEN_EXISTING | FA_READ);
     if (res) return 0;
 
-    res = f_read(&file, (void*) &header, HEADER_SIZE, &bytes_read);
+    res = f_read(&file, (void*) buffer, HEADER_SIZE, &bytes_read);
     if (res || bytes_read < HEADER_SIZE) return 0;
 
     res = f_close(&file);
     if (res) return 0;
+
+    res = parse_header(buffer, &header);
+    if (res != 1) return 0;
 
     return header.version;
 }
@@ -62,8 +72,12 @@ uint8_t firmware_dump(void) {
     FRESULT res;
     FIL file;
     UINT bytes_written;
-    FirmwareHeader_t *header = (FirmwareHeader_t *) HEADER_ADDR;
-    uint32_t file_size = HEADER_SIZE + header->size;
+    FirmwareHeader_t header;
+
+    res = parse_header((uint8_t *) HEADER_ADDR, &header);
+    if (res != 1) return 0;
+
+    uint32_t file_size = HEADER_SIZE + header.size;
 
     memset(&file, 0, sizeof(file));
     res = f_open(&file, CURR_NAME, FA_OPEN_ALWAYS | FA_WRITE);
@@ -92,14 +106,19 @@ uint8_t firmware_basic(void) {
 
 uint8_t firmware_integrity(void) {
     uint32_t crc;
-    FirmwareHeader_t *header = (FirmwareHeader_t *) HEADER_ADDR;
+    uint8_t res;
+    FirmwareHeader_t header;
+
+    res = parse_header((uint8_t *) HEADER_ADDR, &header);
+    if (res != 1) return 0;
+
     uint8_t *addr = (uint8_t *) APP_ADDR;
 
     crc32_init(&crc);
-    crc32_next(&crc, addr, header->size);
+    crc32_next(&crc, addr, header.size);
     crc32_res(&crc);
 
-    return (crc == header->crc) ? 1 : 0;
+    return (crc == header.crc) ? 1 : 0;
 }
 
 void firmware_run(void) {
@@ -145,6 +164,21 @@ static uint8_t upload_file(const char* name) {
 
     res = f_close(&file);
     if (res) return 0;
+
+    return 1;
+}
+
+static uint8_t parse_header(uint8_t *data, FirmwareHeader_t* header) {
+    header->entry_sign = *(data + ENTRY_OFFSET);
+    header->crc = *(uint32_t *) (data + CRC_OFFSET);
+    header->crc_shadow = *(uint32_t *) (data + CRC_SHADOW_OFFSET);
+    header->size = *(uint32_t *) (data + SIZE_OFFSET);
+    header->uuid = *(uint32_t *) (data + UUID_OFFSET);
+    header->version = *(uint16_t *) (data + VERSION_OFFSET);
+    header->closing_sign = *(data + CLOSE_OFFSET);
+
+    if (header->entry_sign != ENTRY_VALUE) return 0;
+    if (header->closing_sign != CLOSE_VALUE) return 0;
 
     return 1;
 }
--- firmware_conf.h
+++ firmware_conf.h
@@ -9,9 +9,19 @@ typedef struct {
     uint32_t uuid;          //  Integer representing unique firmware ID
     uint16_t version;       //  Integer representing firmware version
     uint8_t closing_sign;
-} FirmwareHeader_t;         // 24 bytes
+} FirmwareHeader_t;
 
-#define HEADER_SIZE sizeof(FirmwareHeader_t)
+#define ENTRY_VALUE 0x7E
+#define CLOSE_VALUE 0xFE
+
+#define ENTRY_OFFSET 0
+#define CRC_OFFSET (ENTRY_OFFSET + sizeof(uint8_t))
+#define CRC_SHADOW_OFFSET (CRC_OFFSET + sizeof(uint32_t))
+#define SIZE_OFFSET (CRC_SHADOW_OFFSET + sizeof(uint32_t))
+#define UUID_OFFSET (SIZE_OFFSET + sizeof(uint32_t))
+#define VERSION_OFFSET (UUID_OFFSET + sizeof(uint32_t))
+#define CLOSE_OFFSET (VERSION_OFFSET + sizeof(uint16_t))
+#define HEADER_SIZE (CLOSE_OFFSET + sizeof(uint8_t))
 
 #define DRIVE_NO 0
 #define NEW_NAME "new.bin"
